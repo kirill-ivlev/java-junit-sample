50b566fe91698eee01445f4042c4a612fe7bff96
Liliia Sabitova <107196662+LiliaSabitova@users.noreply.github.com>
2022-10-25 17:39:01 +0200

    Deleted from wrong directory

--- batch/scripts/setEnvVar.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Setting environment variable TESTVAR with value %1
-set testvar=%1
-echo Value of TESTVAR is %testvar%
-pause
--- batch/scripts/testWithStderr.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Test with stderr.
-
-echo throw error into sderr 1>&2
-
-pause
